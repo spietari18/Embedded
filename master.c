0a0571e6f1587063fcd0255539917a92d43a62d7
Samuli Pietarinen <samuli.pietarinen@iki.fi>
2022-03-09 11:13:42 +0200

    Added code base for both master and slave.

---
+++ master.c
@@ -0,0 +1,17 @@
+/* master.c
+ *
+ * Created: 09/03/2022
+ * Author: Samuli, Robert
+ * Using Arduino UNO as master
+ * ATmega328p
+ */
+
+#include <avr/io.h>
+
+int main(void) {
+    // SETUP
+
+    while(1) {
+        // LOOP
+    }
+}
---
+++ slave.c
@@ -0,0 +1,17 @@
+/* slave.c
+ *
+ * Created: 09/03/2022
+ * Author: Samuli, Robert
+ * Using Arduino MEGA as slave
+ * Atmega2560
+ */
+
+#include <avr/io.h>
+
+int main(void) {
+    // SETUP
+
+    while(1) {
+        // LOOP
+    }
+}
