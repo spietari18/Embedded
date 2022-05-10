678113a661024db48dafffee546d4d50e64c0af1
Mnemosyn3 <robert.hamalainen@iki.fi>
2022-05-11 00:33:27 +0300

    Added code for the LCD

--- slave.c
+++ slave.c
@@ -5,14 +5,21 @@
  * Using Arduino MEGA as slave
  * Atmega2560
  */
-
+#define F_CPU 16000000UL
 #include <avr/io.h>
-#include "alarmstates.h";
+#include "alarmstates.h"
+
+#include "lcd.h" // lcd header file made by Peter Fleury
+
 
 int main(void) {
     // SETUP
     // DEFINE VARIABLES
+    lcd_init(LCD_DISP_ON);
+    lcd_clrscr();
 
+
+    int alarm = 0;
     // SETUP PINS
     
     // INITIALIZE I2C OR SPI
@@ -23,25 +30,33 @@ int main(void) {
         // LISTEN FOR MASTER
 
         // IF MESSAGE
-
+            lcd_clrscr()
             // STATE MACHINE TO HANDLE SIGNALS
             switch(message) {
                 case ALARM_OFF:
+                    lcd_puts("Disarmed");
+                    alarm = 0;
                     break;
 
                 case ALARM_ON:
+                    lcd_puts("Armed");
                     break;
 
                 case ALARM_TRIGGER:
+                    lcd_puts("Alarm triggered");
+                    alarm = 1;
                     break;
                 
                 case PW_OK:
+                    lcd_puts("Password Accepted");
                     break;
 
                 case PW_TIMEOUT:
+                    lcd_puts("Timeout");
                     break;
 
                 case PW_WRONG:
+                    lcd_puts("Password Declined");
                     break;
 
                 default:
